afdd56dad2621728803fd0c3e910c9cd2abe13f2
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2019-07-29 14:35:52 -0700

    called the api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,89 +1,27 @@
 
 import React, { Component } from 'react';
-import axios from 'axios'
+import Show from './Show';
 import './App.css';
 
 
-class safeHouse extends Component {
-
-  state = {
-    // Your required states here
-    preliminary_risk_category: " ",
-    neighborhood: " ",
-    adress: " ",
-    city : " ",
-    zip_code: " ",
-    state: " ",
-    year_built:" ",
-    no_stories: " ",
-    building_use : " ",
-    estimated_number_of_occupants: " "
-        
-    
+class App extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  handleChange = (event) => {
-    this.setState({adress: event.target.value});
-    console.log( " your adress is " + this.state.adress);
-  }
-
-  handleSubmit = (event) => {
-    // Your fetch here
-    event.preventDefault()
-    let adress = this.state.adress
-    axios.get(`https://data.seattle.gov/resource/54qs-2h7f.json?address=${this.state.adress}`).then(result =>{
-    
-
-      let preliminary_risk_category = result.data.preliminary_risk_category;
-      let neighborhood = result.data.neighborhood;
-      let adress = result.data.adress;
-      let city = result.data.city;
-      let zip_code = result.data.zip_code;
-      let state = result.data.state;
-      let year_built = result.data.year_built;
-      let  no_stories = result.data.no_stories;
-      let building_use = result.data.building_use;
-      let estimated_number_of_occupants = result.data.estimated_number_of_occupants;
-        
-        this.setState({
-          preliminary_risk_category,
-          neighborhood,
-          adress,
-          city ,
-          zip_code,
-          state,
-          year_built,
-          no_stories,
-          building_use ,
-          estimated_number_of_occupants
-        })
-    })
-    // Your state updates go under function(json)
-  }
-
-  render() {
-    let adress = this.state.adress
-    let city = this.state.city
-    return (
-      <div className = 'container'>
-        <form onSubmit={this.handleSubmit}>
-        <p>
-          
-        <div >
-          <h4>{adress}</h4>
-          
-        </div>
-            </p>
-            <label>
-              Please enter your adress:
-              <input type="text" onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Look up Risk!" />
-          </form>
-      </div>
-    )
-  }
+  render(){
+      return(
+        <div className='Container'>
+            <Show></Show>
+          </div>
+  
+      )
+    }
 }
+export default App
 
 
-export default safeHouse;
+
+
+
+
--- client/src/Show.jsx
+++ client/src/Show.jsx
@@ -1,16 +1,105 @@
-import React from 'react';
+import React,{Component} from 'react';
+import axios from 'axios'
 import './App.css';
 
-function Show() {
-    return (
-      <div className="App">
-        <h3>Your Home's Risk</h3>
-            <h5>Address:</h5>
-            <p>{props.address}</p>
-            <h5>Zip Code:</h5>
-            <p>{props.zipcode}</p>
-      </div>
-    );
-  }
+class Show extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    // Your required states here
+        preliminary_risk_category: " ",
+        neighborhood: " ",
+        address: " ",
+        city : " ",
+        zip_code: " ",
+        state: " ",
+        year_built:" ",
+        no_stories: " ",
+        building_use : " ",
+        estimated_number_of_occupants: " "
+    }
+}
 
-  export default Show;
+handleChange = (event) => {
+  this.setState({address: event.target.value});
+  console.log( " your adress is " + this.state.adress);
+}
+
+handleSubmit = (event) => {
+  // Your fetch here
+  event.preventDefault()
+  let address = this.state.address
+  axios.get(`https://data.seattle.gov/resource/54qs-2h7f.json?address=${this.state.address}`).then(result =>{
+  
+
+    let preliminary_risk_category = result.data[0].preliminary_risk_category;
+    let neighborhood = result.data[0].neighborhood;
+    let address = result.data[0].adress;
+    let city = result.data[0].city;
+    let zip_code = result.data[0].zip_code;
+    let state = result.data[0].state;
+    let year_built = result.data[0].year_built;
+    let  no_stories = result.data[0].no_stories;
+    let building_use = result.data[0].building_use;
+    let estimated_number_of_occupants = result.data[0].estimated_number_of_occupants;
+      
+      this.setState({
+        preliminary_risk_category,
+        neighborhood,
+        address,
+        city ,
+        zip_code,
+        state,
+        year_built,
+        no_stories,
+        building_use ,
+        estimated_number_of_occupants
+      })
+  })
+  // Your state updates go under function(json)
+}
+
+render() {
+  let address = this.state.address
+  let neighborhood = this.state.neighborhood
+  let city = this.state.city
+  let state = this.state.state
+  let zip_code = this.state.zip_code
+  let preliminary_risk_category = this.state.preliminary_risk_category
+  return (
+    <div className = 'container'>
+      <form onSubmit={this.handleSubmit}>
+          
+          <label>
+            Please enter your adress:
+            <input type="text" onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Look up Risk!" />
+        <div >
+          <h4>{address}</h4>
+          <h4>{neighborhood}</h4>
+          <h4>{city}</h4>
+          <h4>{state}</h4>
+          <h4>{zip_code}</h4>
+          <h4>{preliminary_risk_category}</h4>
+        </div>
+        </form>
+    </div>
+  )
+}
+}
+
+
+export default Show;
+// function Show() {
+//   return (
+//     <div className="App">
+//       <h3>Your Home's Risk</h3>
+//           <h5>Address:</h5>
+//           <p>{props.address}</p>
+//           <h5>Zip Code:</h5>
+//           <p>{props.zipcode}</p>
+//     </div>
+//   );
+// }
+// export default Show;
