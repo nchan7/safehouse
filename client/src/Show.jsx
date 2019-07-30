73015e83745bb128a1f53545ae8370733ef2b3d5
Guelito1 <mflorescacravens@gmail.com>
2019-07-30 09:57:55 -0700

    changed comment page

--- client/src/Show.jsx
+++ client/src/Show.jsx
@@ -30,7 +30,6 @@ class Show extends Component {
     console.log( " your adress is " + this.state.adress);
   }
 
-<<<<<<< HEAD
 handleSubmit = (event) => {
   // Your fetch here
   event.preventDefault()
@@ -49,25 +48,6 @@ handleSubmit = (event) => {
     let building_use = result.data[0].building_use;
     let estimated_number_of_occupants = result.data[0].estimated_number_of_occupants;
       
-=======
-  handleSubmit = (event) => {
-    // Your fetch here
-    event.preventDefault()
-    let address = this.state.address
-    axios.get(`https://data.seattle.gov/resource/54qs-2h7f.json?address=${this.state.address}`).then(result =>{
-    
-
-      let preliminary_risk_category = result.data[0].preliminary_risk_category;
-      let neighborhood = result.data[0].neighborhood;
-      let address = result.data[0].adress;
-      let city = result.data[0].city;
-      let zip_code = result.data[0].zip_code;
-      let state = result.data[0].state;
-      let year_built = result.data[0].year_built;
-      let  no_stories = result.data[0].no_stories;
-      let building_use = result.data[0].building_use;
-      let estimated_number_of_occupants = result.data[0].estimated_number_of_occupants;
->>>>>>> 574393a6224b106aff608c29881c3471a985cafb
       this.setState({
         preliminary_risk_category,
         neighborhood,
@@ -85,47 +65,12 @@ handleSubmit = (event) => {
   }
 
 render() {
-<<<<<<< HEAD
   let address = this.state.address
   let neighborhood = this.state.neighborhood
   let city = this.state.city
   let state = this.state.state
   let zip_code = this.state.zip_code
   let preliminary_risk_category = this.state.preliminary_risk_category
-=======
-    let address = this.state.address
-    let year_built = this.state.year_built
-    let neighborhood = this.state.neighborhood
-    let city = this.state.city
-    let state = this.state.state
-    let zip_code = this.state.zip_code
-    let preliminary_risk_category = this.state.preliminary_risk_category
-
-
-  var contents
-  if ( preliminary_risk_category === 'Critical Risk'){
-    contents=(
-      <img src={img4} alt="really bad"/>
-    );
-  }else if (preliminary_risk_category ==='High Risk'){
-    contents=(
-      <img src={img3} alt='high risk' />
-    );
-  }else if (preliminary_risk_category === 'Medium Risk') {
-    contents=(
-      <img src = {img2} alt='medium risk' />
-    );
-  }else if (preliminary_risk_category === " ") {
-    contents=(
-      <>
-        <img src ={img1} alt = 'nodata'/>
-        <p>No Data Available</p>
-      </>
-    );
-  }
-
-
->>>>>>> 574393a6224b106aff608c29881c3471a985cafb
   return (
     <div className = 'container'>
       <form onSubmit={this.handleSubmit}>
@@ -143,10 +88,6 @@ render() {
           <h4>{state}</h4>
           <h4>{zip_code}</h4>
           <h4>{preliminary_risk_category}</h4>
-<<<<<<< HEAD
-=======
-          <div>{contents}</div>
->>>>>>> 574393a6224b106aff608c29881c3471a985cafb
         </div>
         
         </form>
